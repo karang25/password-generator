953cea60e55c44237a95d2d8dc46999c494e1d11
homosapian <evolvingsince2000@gmail.com>
2025-08-05 20:51:48 +0530

    my commit

---
+++ index.js
@@ -0,0 +1,55 @@
+//Random Password Generator
+function code(){
+function generatePassword(passwordLength,includeLowercase,includeUppercase,includeNumbers,includeSymbols){
+
+    const lowercaseChars="abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars="0123456789";
+    const symbolChars="`!@#$%^&*()_+-=[]~";
+
+    let allowedChars="";
+    let password="";
+
+    allowedChars+=includeLowercase? lowercaseChars :"";
+    allowedChars+=includeUppercase? uppercaseChars :"";
+    allowedChars+=includeNumbers? numberChars :"";
+    allowedChars+=includeSymbols? symbolChars :"";
+
+    if(passwordLength<=0){
+        console.log("password length must be atleast 1");
+    }
+    if(allowedChars.length===0){
+        console.log("at least 1 set of characters needs to be selected");
+    }
+
+    for(let i=0;i<passwordLength;i++){
+        const randomIndex= Math.floor(Math.random()*allowedChars.length)+1;
+        password+=allowedChars[randomIndex];
+    }
+
+    console.log(allowedChars);
+
+   
+   
+
+    return password;
+    
+    
+}
+
+
+const passwordLength=12;
+const includeLowercase =true;
+const includeUppercase=true;
+const includeNumbers=true;
+const includeSymbols=true;
+
+
+const Password=generatePassword(passwordLength,
+                                includeLowercase,
+                                includeUppercase,
+                                includeNumbers,
+                                includeSymbols);
+
+console.log(`Generated Password: ${Password}`); 
+document.getElementById("Password").textContent=`Password = ${Password}`;  }                            
